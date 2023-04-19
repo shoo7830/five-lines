b0b7e5f50faaeb65c99506da5ecff04dc69d1d02
Aria Kim <shoo7830@gmail.com>
2023-04-20 00:27:50 +0900

    refactor: getFalling 개선

--- index.ts
+++ index.ts
@@ -176,20 +176,23 @@ class Lock implements Tile {
 // 1. getter 비공개 설정
 // 2. 클래스로 코드 이관
 // 3. 새로운 메서드 삭제 
+// 4. if 문에서 else를 사용하지 말것
 class FallStrategy {
   constructor(private falling: FallingState) { }
   update(tile: Tile, x: number, y: number) {
     this.falling = map[y + 1][x].isAir() ? new Falling() : new Resting();
     this.drop(tile, x, y);
   }
-  moveHorizontal(tile: Tile, dx: number) {
-    this.falling.moveHorizontal(tile, dx);
-  }
+  
   private drop(tile: Tile, x: number, y: number) {
     if (this.falling.isFalling()) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
     }
+  }
+
+  moveHorizontal(tile: Tile, dx: number) {
+    this.falling.moveHorizontal(tile, dx);
   }
 }
 
